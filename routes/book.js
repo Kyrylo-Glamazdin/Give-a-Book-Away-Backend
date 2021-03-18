b1f15a77195e5e76dacd4a6e63c7a6bbdfdca7ac
Kyrylo Glamazdin <kyrylo.glamazdin36@myhunter.cuny.edu>
2021-03-18 11:45:16 -0400

    added user routes and seed database function

--- routes/book.js
+++ routes/book.js
@@ -6,6 +6,9 @@ const {Book} = require('../db/models');
 
 router.get('/', (request, response, next) => {
     //RECOMMENDED BOOKS BASED ON LOCATION? (for the main page)
+    Book.findAll()
+    .then(book => response.status(200).json(book))
+    .catch(err => next(err))
 })
 
 router.get('/key', (request, response, next) => {
@@ -20,7 +23,7 @@ router.get('/:id', (request, response, next) => {
     .catch(err => console.log(err))
 })
 
-router.post('/', (requset, response, next) => {
+router.post('/', (request, response, next) => {
     Book.create(request.body)
     .then(book => response.status(200).json(book))
     .catch(err => next(err));
