37e841b0ebfb8dd2f411a27819b2181793d4013f
Kyrylo Glamazdin <kyrylo.glamazdin36@myhunter.cuny.edu>
2021-04-14 15:39:59 -0400

    started chat setup

--- app.js
+++ app.js
@@ -2,11 +2,26 @@ const express = require('express');
 const app = express();
 const db = require('./db');
 const httpRouter = require('./routes/index.js');
+const socketIo = require('socket.io');
 
 const makeDatabase = require('./functions/makeDatabase');
 const seedDatabase = require('./functions/seedDatabase');
 
+const http = require('http');
+const server = http.createServer(app);
+const io = require('socket.io')(server, {
+    cors: {
+      origin: '*',
+    }
+  });
+
+const cors = require('cors');
+
 const PORT = 3500;
+
+io.on('connection', socket => {
+    console.log("New connection")
+})
 
 const syncDB = async() => {
     try {
@@ -29,18 +44,20 @@ const syncDB = async() => {
 }
 
 const utilities = async() => {
-    app.use((req, res, next) => {
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.setHeader('Access-Control-Allow-Methods', '*');
-        res.setHeader('Access-Control-Allow-Headers', "*");
-        next();
-    });
+    // app.use((req, res, next) => {
+    //     res.setHeader("Access-Control-Allow-Origin", "*");
+    //     res.setHeader('Access-Control-Allow-Methods', '*');
+    //     res.setHeader('Access-Control-Allow-Headers', "*");
+    //     next();
+    // });
+
+    app.use(cors());
 
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
 
     app.use("/api", httpRouter);
-    app.listen(PORT, () => {
+    server.listen(PORT, () => {
         console.log(`Running on port ${PORT}`);
     })
 }
